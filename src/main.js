63088880482fce407f0ed1af0b7f261d584c474d
LucArte <96082778+Lucarte@users.noreply.github.com>
2023-06-02 16:46:47 +0200

    mehr page ready for mobile and web

--- src/main.js
+++ src/main.js
@@ -1,178 +1,112 @@
 'use strict'
 
-// - // - // - // - // - // -  MOUSE EFFECT  - // - // - // - // - // - // -
-
-let circle = document.getElementById('circle');
-
-document.addEventListener('mousemove', function (e) {
-  let x = e.clientX;
-  let y = e.clientY;
-
-  circle.style.left = x - (circle.offsetWidth / 2) + 'px';
-  circle.style.top = y - (circle.offsetHeight / 2) + 'px';
-});
-
-
 // - // - // - // - // - // -  FORM VALIDATION - // - // - // - // - // - // -
 
-(function () {
-  let form = document.querySelector('#register-form');
-  let emailInput = document.querySelector('#email');
-  let nameInput = document.querySelector('#name');
+let form = document.querySelector('#register-form');
+let nameInput = document.querySelector('#name');
+let email = document.querySelector('#email');
+let message = document.querySelector('#message');
+let anrede = document.querySelector('#anrede');
 
-  function showErrorMessage(input, message) {
-    let container = input.parentElement;
+function showErrorMessage(input, message) {
+  let container = input.parentElement;
+  let error = container.querySelector('.error-message');
 
-    let error = container.querySelector('.error-message');
-    if (error) {
-      container.removeChild(error);
-    }
-
-    if (message) {
-      let error = document.createElement('div');
-      error.classList.add('error-message');
-      error.innerText = message;
-      container.appendChild(error);
-    }
+  if (error) {
+    container.removeChild(error);
   }
 
-  function validateName() {
-    let value = nameInput.value;
-    if (!value) {
-      showErrorMessage(nameInput, 'Name is a required field');
-      return false;
-    }
+  if (message) {
+    error = document.createElement('div');
+    error.classList.add('error-message');
+    error.innerText = message;
+    container.appendChild(error);
+  }
+}
 
-    if (value.length < 2) {
-      showErrorMessage(nameInput, 'Name must be at least 2 characters long.');
-      return false;
-    }
-
-    showErrorMessage(nameInput, null);
-    return true;
+function validateAnrede() {
+  let value = anrede.value;
+  if (value.length === 0) {
+    showErrorMessage(anrede, 'Wahl erforderlich!');
+    return false;
   }
 
-  function validateEmail() {
-    let value = emailInput.value;
+  showErrorMessage(anrede, null);
+  return true;
+}
 
-    if (!value) {
-      showErrorMessage(emailInput, 'Email is a required field');
-      return false;
-    }
-
-    // if (value.indexOf('@' && '.') === -1) {
-    //   showErrorMessage(emailInput, 'Please enter e valid email address.');
-    //   return false;
-    // }
-
-    if (value.indexOf('@') === -1) {
-      showErrorMessage(emailInput, 'Please enter e valid email address.');
-      return false;
-    }
-
-    if (value.indexOf('.') === -1) {
-      showErrorMessage(emailInput, 'Please enter a valid email address.');
-      return false;
-    }
-
-    showErrorMessage(emailInput, null);
-    return true;
+function validateNameInput() {
+  let value = nameInput.value.trim();
+  if (!value) {
+    showErrorMessage(nameInput, 'Name is a required field');
+    return false;
+  } else if (!/^[A-Za-zÀ-ÖØ-öø-ÿ]+$/.test(value)) {
+    showErrorMessage(nameInput, 'Nur Buchstaben sind erlaubt');
+    return false;
+  } else if (value.length === 1) {
+    showErrorMessage(nameInput, 'Zu kurz! Mindestes 2 Buchstaben sind erforderlich');;
+    return false;
+  } else if (value.length > 15) {
+    showErrorMessage(nameInput, 'Zu lang! Maximal 15 Buchstaben');;
+    return false;
   }
 
-  function validateForm() {
-    let isValidName = validateName();
-    let isValidEmail = validateEmail();
+  showErrorMessage(nameInput, null);
+  return true;
+}
 
-    return isValidName && isValidEmail;
+
+function validateEmail() {
+  let value = email.value.trim();
+
+  if (!value) {
+    showErrorMessage(email, 'Feld darf nicht leer sein');
+    return false;
+  } else if (value.indexOf('@' && '.') === -1) {
+    showErrorMessage(email, 'Invalide E-Mail adresse.');
+    return false;
+  } else if (value.indexOf('@') === -1) {
+    showErrorMessage(email, 'E-Mail Adresse muss ein @ enthalten.');
+    return false;
+  } else if (value.indexOf('.') === -1) {
+    showErrorMessage(email, 'E-Mail Adresse muss ein Punkt enthalten.');
+    return false;
   }
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault(); // ordering NOT to submit to the server
-    if (validateForm()) {
-      alert('SUCSESS!!!');
-    }
-  })
-})();
+  showErrorMessage(email, null);
+  return true;
+}
+
+function validateMessage() {
+  let value = message.value.trim();
+
+  if (value.length < 2) {
+    showErrorMessage(message, 'Feld erforderlich!')
+    return false;
+  }
+
+  showErrorMessage(message, null);
+  return true;
+}
+
+function validateForm() {
+  let isValidNameInput = validateNameInput();
+  let isValidEmail = validateEmail();
+  let isValidMessage = validateMessage();
+  let isValidAnrede = validateAnrede();
+
+  return isValidAnrede && isValidNameInput && isValidEmail && isValidMessage;
+}
 
 
+anrede.addEventListener('focusout', validateAnrede);
+nameInput.addEventListener('focusout', validateNameInput);
+email.addEventListener('focusout', validateEmail);
+message.addEventListener('focusout', validateMessage);
 
-
-// FOR ANUK!!!
-
-
-//-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ CONTACT FORM
-
-// (function () {
-//   let application = document.querySelector('.contact-form');
-//   const nameInput = document.querySelector('#name');
-//   const emailInput = document.querySelector('#email');
-//   console.log(nameInput, emailInput);
-
-//   function showErrorMessage(input, message) {
-//     let container = input.parentElement;
-//     let error = container.querySelector('.error-message');
-
-//     if (error) {
-//       container.removeChild(error);
-//     }
-
-//     if (message) {
-//       let error = document.createElement('div');
-//       error.classList.add('error-message');
-//       error.innerText = message;
-//       container.appendChild(error);
-//     }
-//   }
-
-//   function verifyName() {
-//     let value = nameInput.value;
-//     if (!value) {
-//       showErrorMessage(nameInput, 'Name muss angegeben werden.');
-//       return false;
-//     }
-
-//     if (value.length < 2) {
-//       showErrorMessage(nameInput, 'Name muss mindestens 2 Buchstaben enthalten.');
-//       return false;
-//     }
-
-//     showErrorMessage(nameInput, null);
-//     return true;
-//   }
-
-//   function verifyEmail() {
-//     let value = emailInput.value;
-
-//     if (!value) {
-//       showErrorMessage(emailInput, 'E-Mail muss angegeben werden.');
-//       return false;
-//     }
-
-//     if (value.indexOf('@') === -1) {
-//       showErrorMessage(emailInput, 'Valide E-Mail eingeben.');
-//       return false;
-//     }
-
-//     if (value.indexOf('.') === -1) {
-//       showErrorMessage(emailInput, 'Valide E-Mail eingeben.');
-//       return false;
-//     }
-
-//     showErrorMessage(emailInput, null);
-//     return true;
-//   }
-
-//   function verifyForm() {
-//     let isValidName = verifyName;
-//     let isValidEmail = verifyEmail;
-
-//     return isValidName && isValidEmail;
-//   }
-
-//   application.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     if (verifyForm()) {
-//       alert('GESCHAFFT!')
-//     }
-//   })
-// })();
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); // ordering NOT to submit to the server
+  if (validateForm()) {
+    alert('SUCSESS!!!');
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ['./index.html', './src/mehr.html'],
   theme: {
     extend: {},
+    colors: {
+      veryDarkBrown: '#412f20',
+      darkBrown: '#7e5e45',
+      lightBrown: '#c4a892',
+      brownTransparent: 'rgba(196, 168, 146, .5)',
+      green: '#889830',
+      lightTurquis: '#d7e6e7',
+      darkTurquis: '#c0d5d6',
+      fontLight: '#c0d5d6',
+    },
   },
   plugins: [],
 }
