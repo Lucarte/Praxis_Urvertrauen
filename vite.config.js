954590e85ca6c89c910eddc6637fe19ca7db8877
LucArte <96082778+Lucarte@users.noreply.github.com>
2025-07-17 14:41:59 +0200

    neue bild run

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import ViteSassPlugin from 'vite-plugin-sass';
+import ViteSassPlugin from "vite-plugin-sass";
 
 export default {
-    plugins: [ ViteSassPlugin() ],
-    base: 'https://praxis-urvertrauen.de/Praxis_Urvvertrauen/',
+	plugins: [ViteSassPlugin()],
+	base: "./",
 };
