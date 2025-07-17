5492724a044aa19226dec18313745eca59c62f9d
LucArte <96082778+Lucarte@users.noreply.github.com>
2025-07-17 16:32:01 +0200

    neues Bild

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
+import { defineConfig } from "vite";
 import ViteSassPlugin from "vite-plugin-sass";
 
-export default {
+export default defineConfig({
 	plugins: [ViteSassPlugin()],
-	base: "./",
-};
+	base: "./", // relative Pfade im Build
+	build: {
+		rollupOptions: {
+			input: {
+				main: "./index.html",
+				uebermich: "./src/html/uebermich.html",
+				angebot: "./src/html/angebot.html",
+				mehr: "./src/html/mehr.html",
+				impressum: "./src/html/impressum.html",
+				datenschutz: "./src/html/datenschutz.html",
+			},
+		},
+	},
+});
